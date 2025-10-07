89ea90b37cf10d53007b7bdf271871ef557649e4
HOFF <74277068+1y1c0c8@users.noreply.github.com>
2025-10-07 09:15:39 +0800

    upload files

---
+++ receiver.c
@@ -0,0 +1,22 @@
+#include "receiver.h"
+
+void receive(message_t* message_ptr, mailbox_t* mailbox_ptr){
+    /*  TODO: 
+        1. Use flag to determine the communication method
+        2. According to the communication method, receive the message
+    */
+    
+    // (1) 判斷模式
+    
+}
+
+int main(){
+    /*  TODO: 
+        1) Call receive(&message, &mailbox) according to the flow in slide 4
+        2) Measure the total receiving time
+        3) Get the mechanism from command line arguments
+            • e.g. ./receiver 1
+        4) Print information on the console according to the output format
+        5) If the exit message is received, print the total receiving time and terminate the receiver.c
+    */
+}
---
+++ sender.c
@@ -0,0 +1,25 @@
+#include "sender.h"
+
+void send(message_t message, mailbox_t* mailbox_ptr){
+    /*  TODO: 
+        1. Use flag to determine the communication method
+        2. According to the communication method, send the message
+    */
+    
+    // (1) 
+}
+
+int main(){
+    /*  TODO: 
+        1) Call send(message, &mailbox) according to the flow in slide 4
+        2) Measure the total sending time
+        3) Get the mechanism and the input file from command line arguments
+            • e.g. ./sender 1 input.txt
+                    (1 for Message Passing, 2 for Shared Memory)
+        4) Get the messages to be sent from the input file
+        5) Print information on the console according to the output format
+        6) If the message form the input file is EOF, send an exit message to the receiver.c
+        7) Print the total sending time and terminate the sender.c
+    */
+    
+}
